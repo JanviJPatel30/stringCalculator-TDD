9031a8e7f46b81b8612a3ff8a00688e4d47ad9a0
JanviPatel <janvi30052002@outlook.com>
2025-07-03 20:59:13 +0530

    Make test pass for single number input

--- calculator.js
+++ calculator.js
@@ -1,5 +1,9 @@
 function add(numbers) {
+  // Specification 1: Return 0 for an empty string
   if (numbers === "") return 0;
+
+  // Specification 2: Return the number itself if only one number is given
+  return parseInt(numbers);
 }
 
 module.exports = { add };
